054447f21619300c0371ac7e68de32a989424171
Quentin PROUST <q.proust@gmail.com>
2019-10-02 20:47:13 +0200

    reformat

--- client/src/pages/HomeWrapper.tsx
+++ client/src/pages/HomeWrapper.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import axios from 'axios';
 
-import { Segment, Button, Card, List, Grid } from 'semantic-ui-react'
+import { Segment, Button, Card, List, Grid, Icon, Container, Message } from 'semantic-ui-react'
 
 /****** SERVICE */
 
 const getListSubject = () => {
-    return axios.get(`/api/subjects`)
-        .then((response) => {
-            return response.data;
-        });
+  return axios.get(`/api/subjects`)
+    .then((response) => {
+      return response.data;
+    });
 }
 
 const getListDojo = () => {
-    return axios.get(`/api/dojos`)
-        .then((response) => {
-            return response.data;
-        });
+  return axios.get(`/api/dojos`)
+    .then((response) => {
+      return response.data;
+    });
 }
 
 
@@ -24,107 +24,118 @@ const getListDojo = () => {
 
 
 const toggleVote2 = (id) => {
-    return axios.post(`/api/subjects/${id}/interest`)
-        .then((response) => {
-            return response.data;
-        });
+  return axios.post(`/api/subjects/${id}/interest`)
+    .then((response) => {
+      return response.data;
+    });
 }
 
 
 const ListSubject = ({ subjects, toggleVote }) => {
-    return (
-        <List divided verticalAlign='middle'>
-            {subjects.map(subject =>
-                <Subject key={subject.id} subject={subject} toggleVote={toggleVote} />)}
-        </List>
-    )
+  return (
+    <List divided verticalAlign='middle'>
+      {subjects.map(subject =>
+        <Subject key={subject.id} subject={subject} toggleVote={toggleVote} />)}
+    </List>
+  )
 }
 
 const Subject = ({ subject, toggleVote }) => {
-    return (
-        <List.Item>
-            <List.Content floated='right'>
-            <Button
-        color='green'
-        icon={ 'thumbs up outline'}
-        onClick={() => toggleVote(subject.id)}
-        label={{ basic: true, color: 'green', pointing: 'left', content: subject.interested.length }}
-      />
-            </List.Content>
+  return (
+    <List.Item>
+      <List.Content floated='right'>
+        <Button
+          color='green'
+          icon={'thumbs up outline'}
+          onClick={() => toggleVote(subject.id)}
+          label={{ basic: true, color: 'green', pointing: 'left', content: subject.interested.length }}
+        />
+      </List.Content>
 
-            <List.Content>{subject.theme} </List.Content>
+      <List.Content>{subject.theme} </List.Content>
 
-        </List.Item>
-    )
+    </List.Item>
+  )
 }
 
-const Dojo = ({dojo}) => {
-    return (
- 
-         <Card fluid>
-         <Card.Content>
-           <Card.Header>{dojo.label}</Card.Header>
-           <Card.Meta>{dojo.location}</Card.Meta>
-           <Card.Description>
-             {! dojo.subject && (
-                 <p>Pas de sujet défini, penser à voter </p>
-             )}  
-             
-           </Card.Description>
-         </Card.Content>
-       </Card>
-    )
+const Dojo = ({ dojo }) => {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Card.Header>{dojo.label}</Card.Header>
+        <Card.Meta>{dojo.location}</Card.Meta>
+        <Card.Description>
+          {!dojo.subject && (
+            <Message
+              icon='meh'
+              content={`Le sujet du dojo n'a pas encore été sélectionné. Pensez à voter pour les sujets.`}
+            />
+          )}
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  )
 }
 
 
 export default () => {
 
-    const [subjects, setSubjects] = React.useState([]);
-    const [dojos, setDojos] = React.useState([]);
-    const [error, setError] = React.useState(null);
-    const [reloadplease, setreloadplease] = React.useState(true);
+  const [subjects, setSubjects] = React.useState([]);
+  const [dojos, setDojos] = React.useState([]);
+  const [error, setError] = React.useState(null);
+  const [reloadplease, setreloadplease] = React.useState(true);
 
-    const toggleVote = (id) => {
-        toggleVote2(id)
-        .then ( response =>
-            setreloadplease(!reloadplease) // TODO c'est degueu
-            )
-        
-    }
+  const toggleVote = (id) => {
+    toggleVote2(id)
+      .then(response =>
+        setreloadplease(!reloadplease) // TODO c'est degueu
+      )
 
-    React.useEffect(() => {
-        getListSubject()
-            .then(listeSubject =>            
-                setSubjects(listeSubject)
-            )
-    }, [reloadplease]);
+  }
 
-    React.useEffect(() => {
-        getListDojo()
-            .then(listeDojo =>
-                setDojos(listeDojo)
-            )
-    }, []);
-    
-    const dojo = dojos[0];
-    return (
-        <Grid>
-        <Grid.Row>
-          <Grid.Column width={10}>
-              {dojo && (
-                  <Dojo dojo={dojo} />
-              )}
-              {!dojo && (
-                  <Segment>Pas de dojo prévu</Segment>
-              )}
-              
-          </Grid.Column>
-          <Grid.Column width={6}>
-          <Segment><ListSubject subjects={subjects} toggleVote={toggleVote} /></Segment>
-          </Grid.Column>
-        </Grid.Row>
-        </Grid>
+  React.useEffect(() => {
+    getListSubject()
+      .then(listeSubject =>
+        setSubjects(listeSubject)
+      )
+  }, [reloadplease]);
 
-        
-    )
+  React.useEffect(() => {
+    getListDojo()
+      .then(listeDojo =>
+        setDojos(listeDojo)
+      )
+  }, []);
+
+  const dojo = dojos[0];
+  return (
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={10}>
+          {dojo && (
+            <Dojo dojo={dojo} />
+          )}
+          {!dojo && (
+            <Message
+              icon='meh'
+              content='Pas de dojo prévu pour le moment.'
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={6}>
+          {dojo && (
+            <Segment>
+              <ListSubject subjects={subjects} toggleVote={toggleVote} />
+            </Segment>
+          )}
+          {!dojo && (
+            <Message
+              icon='meh'
+              content={`Pas de sujets de dojo pour le moment. D'autres sujets arriveront bientôt ...`}
+            />
+          )}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
 }
