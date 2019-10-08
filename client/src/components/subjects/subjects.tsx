bc492e45856c8ed57cb6d539c7b7165360bfb5cf
Vincent Gouadon Serlians <vincent.gouadon@serli.com>
2019-10-08 20:38:06 +0200

    UI Improve on Subject item

--- client/src/components/subjects/subjects.tsx
+++ client/src/components/subjects/subjects.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { List, Button } from 'semantic-ui-react';
+import { List, Button, Popup, Image } from 'semantic-ui-react';
 import { UserContext } from '../../user/UserContext';
 
 const ListSubject = ({ subjects, toggleVote }) => {
@@ -10,6 +10,20 @@ const ListSubject = ({ subjects, toggleVote }) => {
     </List>
   )
 }
+
+
+const ListIntertedPers = ({interested}) => {
+  return <>
+  {interested.map(person =>
+    <Popup trigger={<Image src={'https://i.pravatar.cc/' + person} avatar />} flowing hoverable>
+      {person}
+    </Popup>
+
+  )}
+  </>
+}
+
+
 
 const SubjectItem = ({ subject, toggleVote }) => {
   const { user: { sub: yourSub } } = React.useContext(UserContext);
@@ -24,9 +38,9 @@ const SubjectItem = ({ subject, toggleVote }) => {
           label={{ basic: true, color: subject.interested.includes(yourSub) ? 'green' : 'grey', pointing: 'left', content: subject.interested.length }}
         />
       </List.Content>
-
       <List.Content>{subject.theme} </List.Content>
 
+      <List.Content><ListIntertedPers interested={subject.interested}/></List.Content>
     </List.Item>
   )
 }
