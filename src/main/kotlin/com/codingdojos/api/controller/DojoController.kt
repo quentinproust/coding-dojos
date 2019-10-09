16b0415acec76a6015173f8b950305dd0d649fd1
Vincent Gouadon <vincent.gouadon@serli.com>
2019-10-09 19:45:40 +0200

    Ajout d'un endpoint d'affectation d'un sujet au dojo

--- src/main/kotlin/com/codingdojos/api/controller/DojoController.kt
+++ src/main/kotlin/com/codingdojos/api/controller/DojoController.kt
@@ -6,19 +6,23 @@ import com.codingdojos.api.model.DatePoll
 import com.codingdojos.api.model.Dojo
 import com.codingdojos.api.model.Subject
 import com.codingdojos.api.repository.DojoReactiveRepository
+import com.codingdojos.api.repository.SubjectReactiveRepository
 import com.codingdojos.api.service.user.UserInfoService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.util.function.Tuple2
+import java.util.*
 
 
 @RestController
 @RequestMapping("/api/dojos")
 class DojoController @Autowired constructor(
     val dojoRepository: DojoReactiveRepository,
-    val userInfoService: UserInfoService
+    val subjectRepository: SubjectReactiveRepository
 ) {
     val logger = LoggerFactory.getLogger(DojoController::class.java)
 
@@ -46,6 +50,35 @@ class DojoController @Autowired constructor(
     fun create(@RequestBody dojo: Dojo): Mono<Dojo> {
         return dojoRepository.save(dojo)
     }
+
+    @PostMapping(value = ["/{dojoId}/subject/affect/{subjectId}"])
+    fun affectSubject ( @PathVariable dojoId: String, @PathVariable subjectId: String) :Mono<Dojo> {
+        // TODO ONly for admin
+
+        return Mono.zip(dojoRepository.findById(dojoId), subjectRepository.findById(subjectId))
+        { t: Dojo, u: Subject? ->
+            if (u == null) {
+                //exeception 400 l'id xx subject inconnu        TODO
+            }
+            if (t == null) {
+                //exeception 400 l'id xx dojo inconnu           TODO
+            }
+            if (t.subject === null) {
+                //exception 400 le Dojo a deja un sujet            TODO
+            }
+
+            return@zip t.copy(subject = u)
+        }.flatMap {
+            dojoRepository.save(it)
+
+        }.flatMap {dojo ->
+            subjectRepository.deleteById(subjectId).map {
+                return@map dojo
+            }
+
+        }
+    }
+
 
     @PutMapping
     fun save(@RequestBody dojo: Dojo): Mono<Dojo> {
