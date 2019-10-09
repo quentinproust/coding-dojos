363472f33e7abab9db2b8e95dc4170549dc6b0be
Quentin PROUST <q.proust@gmail.com>
2019-10-09 22:28:12 +0200

    persist users

--- src/main/kotlin/com/codingdojos/api/service/user/UserInfo.kt
+++ src/main/kotlin/com/codingdojos/api/service/user/UserInfo.kt
@@ -1,16 +1,44 @@
 package com.codingdojos.api.service.user
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 
 data class UserInfo(
-        val sub: String,
-        val name: String,
-        @get:JsonProperty("given_name") val givenName: String,
+    val sub: String,
+    val name: String,
+    @get:JsonProperty("given_name") val givenName: String,
         @get:JsonProperty("family_name") val familyName: String,
         val profile: String?,
-        val picture: String,
+    val picture: String,
         val email: String,
         @get:JsonProperty("email_verified") val emailVerified: Boolean,
         val isAdmin: Boolean = false,
         val hd: String?
 )
+
+fun mapAuthenticationToUserInfo(auth: Authentication): UserInfo {
+    return when (auth) {
+        is OAuth2AuthenticationToken -> {
+            LoggerFactory.getLogger(UserInfo::class.java).info("User info : {}", auth)
+            mapAttributesToUserInfo(auth)
+        }
+        else -> throw RuntimeException("$auth is not supported")
+    }
+}
+
+private fun mapAttributesToUserInfo(auth: OAuth2AuthenticationToken): UserInfo {
+    val attributes = auth.principal.attributes
+    return UserInfo(
+        sub = attributes["sub"] as String,
+        name = attributes["name"] as String,
+        profile = attributes["profile"] as String?,
+        picture = attributes["picture"] as String,
+        email = attributes["email"] as String,
+        emailVerified = attributes["email_verified"] as Boolean,
+        hd = attributes["hd"] as String?,
+        familyName = "",
+        givenName = ""
+    )
+}
